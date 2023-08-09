dbe41c35a60185cc85c22f9d8a9e54ff5110ccc6
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-09 20:08:12 +0100

    Update experiment script

--- examples/rl/train_rl_model.sh
+++ examples/rl/train_rl_model.sh
@@ -19,17 +19,20 @@ fi
 # Train the unsafe controller/agent.
 for seed in 0 1 2 3 4
 do 
-    for safety_coef in 0 1 10 
+    for safety_coef in 10  
     do
-        python3 ../../safe_control_gym/experiments/execute_rl_controller.py \
-            --algo ${ALGO} \
-            --task ${SYS_NAME} \
-            --overrides \
-                ./config_overrides/${SYS}/${ALGO}_${SYS}.yaml \
-                ./config_overrides/${SYS}/${SYS}_${TASK}.yaml \
-            --output_dir ./ \
-            --tag ${ALGO}_${SYS}_${TASK}/ \
-            --seed $seed \
-            --kv_overrides algo_config.safety_coef=$safety_coef wandb.group='expt'
+        for bounded in True False
+        do 
+            python3 ../../safe_control_gym/experiments/execute_rl_controller.py \
+                --algo ${ALGO} \
+                --task ${SYS_NAME} \
+                --overrides \
+                    ./config_overrides/${SYS}/${ALGO}_${SYS}.yaml \
+                    ./config_overrides/${SYS}/${SYS}_${TASK}.yaml \
+                --output_dir ./ \
+                --tag ${ALGO}_${SYS}_${TASK}_bounded=${bounded}/ \
+                --seed $seed \
+                --kv_overrides algo_config.safety_coef=$safety_coef wandb.group='ablations'
+        done
     done
 done
